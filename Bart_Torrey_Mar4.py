aa7ab74269016fd987e1f236862e16108629b7a4
Torrey-Rhyne-UCSD <151965894+Torrey-Rhyne-UCSD@users.noreply.github.com>
2024-03-04 11:56:07 -0800

    Update Bart_Torrey_Mar4.py
    fix lock issue?

--- Bart_Torrey_Mar4.py
+++ Bart_Torrey_Mar4.py
@@ -67,9 +67,9 @@ def main():
     parser = argparse.ArgumentParser(description="Motif Finder Script")
     parser.add_argument("-i", "--input", required=True, help="Input sequences file (FASTA format)")
     parser.add_argument("-o", "--output", required=True, help="Output results file")
-    parser.add_argument("-m", "--motif", required=True, help="Motif file (JASPAR format)")
-    parser.add_argument("-t", "--score_threshold", type=float, default=0.05, help="Top scores threshold")
-    parser.add_argument("-l", "--output_limit", type=int, default=500, help="Output limit for the number of hits")
+    parser.add_argument("-m", "--motif", required=True, help="Input motif file (JASPAR format)")
+    parser.add_argument("-t", "--score_threshold", type=float, default=0.05, help="Top scores threshold (default = 0.05)")
+    parser.add_argument("-l", "--output_limit", type=int, default=500, help="Output limit for the number of hits (default = 500)")
     args = parser.parse_args()
 
     input_sequences = SeqIO.to_dict(SeqIO.parse(args.input, "fasta"))
